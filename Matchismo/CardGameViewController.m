bc614d604cd55f6dc61481f509ddc4c3f56245b4
Rob Rogers <rrogers@Roberts-MacBook-Air.local>
2013-02-03 21:26:38 -0800

    updated to do two things: 1) show 12 buttons of cards, 2) create the setter for the Outlet collection NSARRAY of 12 buttons, this sets the card.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -8,41 +8,35 @@
 
 #import "CardGameViewController.h"
 #import "PlayingCardDeck.h"
-#import "PlayingCard.h"
+
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property (strong,nonatomic) PlayingCardDeck *deck;
+@property (strong,nonatomic) Deck *deck;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @end
 
 @implementation CardGameViewController
 
--(PlayingCardDeck *)deck {
+-(Deck *)deck {
     if (!_deck) {
         _deck = [[PlayingCardDeck alloc] init];
     }
     return _deck;
 }
-
+-(void)setCardButtons:(NSArray *)cardButtons {
+    _cardButtons = cardButtons;
+    for (UIButton *cardButton in cardButtons) {
+        Card *card = [self.deck drawRandomCard];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+    }
+}
 -(void)setFlipCount:(int)flipCount {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
 }
 - (IBAction)flipCard:(UIButton *)sender {
-    Card *card;
-    
-    if (!sender.selected) {
-        card = (PlayingCard *)[self.deck drawRandomCard];
-        if (card) {
-            [sender setTitle: card.contents forState: UIControlStateSelected];
-            NSLog(@"card %@", card.contents);
-        } else {
-            [sender setTitle: @"?" forState: UIControlStateSelected];
-        }
-
-    }
-
     sender.selected = !sender.selected;
     self.flipCount++;
 }
